abc5d41675d0e893a872360069caa8317502840f
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-05-18 15:53:28 +0200

    Add test cases

---
+++ src/test/java/no/ssb/dapla/blueprint/parser/NotebookFileVisitorTest.java
@@ -0,0 +1,16 @@
+package no.ssb.dapla.blueprint.parser;
+
+import org.junit.jupiter.api.Test;
+
+class NotebookFileVisitorTest {
+
+    @Test
+    void testIgnoringFolders() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void testOnlyIncludesIpynb() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/no/ssb/dapla/blueprint/parser/NotebookProcessorTest.java
@@ -0,0 +1,26 @@
+package no.ssb.dapla.blueprint.parser;
+
+import org.junit.jupiter.api.Test;
+
+class NotebookProcessorTest {
+
+    @Test
+    void testCanParseNotebook() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void testHandlesWeirdCharacters() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void testSupportsMissingMetadata() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void testSupportsEmptyMetadata() {
+        throw new UnsupportedOperationException();
+    }
+}
