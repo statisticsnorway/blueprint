9f6b6c7749124b324978b547110085e69b160199
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-05-26 11:16:52 +0200

    Use embedded database

--- src/test/java/no/ssb/dapla/blueprint/parser/ParserTest.java
+++ src/test/java/no/ssb/dapla/blueprint/parser/ParserTest.java
@@ -1,11 +1,36 @@
 package no.ssb.dapla.blueprint.parser;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.neo4j.configuration.connectors.BoltConnector;
+import org.neo4j.configuration.helpers.SocketAddress;
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 class ParserTest {
+
+    private static DatabaseManagementService managementService;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        Path tempFile = Files.createTempDirectory("dapla-blueprint-db-");
+        tempFile.toFile().deleteOnExit();
+        managementService = new DatabaseManagementServiceBuilder(tempFile.toFile())
+                .setConfig(BoltConnector.enabled, true)
+                .setConfig(BoltConnector.listen_address, new SocketAddress("localhost", 7687))
+                .build();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        managementService.shutdown();
+    }
 
     @Test
     void testCommit1() throws IOException {
