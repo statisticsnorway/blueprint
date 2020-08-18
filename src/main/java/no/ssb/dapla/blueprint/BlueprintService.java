d63b3c86be16e8ffff4b5998d80e45d4b4997f62
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-08-18 09:28:13 +0200

    Implement repository endpoints

--- src/main/java/no/ssb/dapla/blueprint/BlueprintService.java
+++ src/main/java/no/ssb/dapla/blueprint/BlueprintService.java
@@ -3,12 +3,13 @@ package no.ssb.dapla.blueprint;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.helidon.common.http.Http;
+import io.helidon.common.http.MediaType;
 import io.helidon.config.Config;
-import io.helidon.webserver.Routing;
-import io.helidon.webserver.ServerRequest;
-import io.helidon.webserver.ServerResponse;
-import io.helidon.webserver.Service;
-import no.ssb.dapla.blueprint.git.GitHookService;
+import io.helidon.webserver.*;
+import no.ssb.dapla.blueprint.notebook.Dependency;
+import no.ssb.dapla.blueprint.notebook.Notebook;
+import no.ssb.dapla.blueprint.notebook.Repository;
+import no.ssb.dapla.blueprint.notebook.Revision;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
@@ -16,12 +17,25 @@ import org.neo4j.driver.summary.ResultSummary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-import java.util.concurrent.CompletionStage;
+import java.util.List;
 
+import static io.helidon.common.http.MediaType.APPLICATION_JSON;
 import static org.neo4j.driver.Values.parameters;
 
 public class BlueprintService implements Service {
+
+    static final MediaType APPLICATION_REPOSITORY_JSON = MediaType.create(
+            "application", "vnd.ssb.blueprint.repository+json");
+
+    static final MediaType APPLICATION_NOTEBOOK_JSON = MediaType.create(
+            "application", "vnd.ssb.blueprint.notebook+json");
+
+    static final MediaType APPLICATION_REVISION_JSON = MediaType.create(
+            "application", "vnd.ssb.blueprint.revision+json");
+
+    static final MediaType APPLICATION_DAG_JSON = MediaType.create(
+            "application", "vnd.ssb.blueprint.dag+json");
+
 
     private static final Logger LOG = LoggerFactory.getLogger(BlueprintService.class);
 
@@ -33,13 +47,120 @@ public class BlueprintService implements Service {
         this.driver = driver;
     }
 
+    private static RequestPredicate.ConditionalHandler withAccept(Handler handler, MediaType... mediaTypes) {
+        return RequestPredicate.create()
+                .accepts(mediaTypes)
+                .thenApply(handler);
+    }
+
+    private static Handler withAcceptAndJson(Handler handler, MediaType mediaType) {
+        return withAccept(handler, APPLICATION_JSON, mediaType)
+                .otherwise((req, res) -> {
+                    throw new HttpException("invalid content-type", Http.Status.NOT_ACCEPTABLE_406);
+                });
+    }
+
     @Override
     public void update(Routing.Rules rules) {
         rules
+                .get("/repository",
+                        withAcceptAndJson(this::getRepositoriesHandler, APPLICATION_REPOSITORY_JSON))
+                .get("/repository/{repoID}/revisions", withAcceptAndJson(this::getRevisionsHandler, APPLICATION_REVISION_JSON))
+
+                .get("/revisions/{revID}/notebooks", RequestPredicate.create()
+                        .accepts(APPLICATION_NOTEBOOK_JSON, APPLICATION_JSON).thenApply(this::getNotebooksHandler))
+                .get("/revisions/{revID}/notebooks", RequestPredicate.create()
+                        .accepts(APPLICATION_DAG_JSON).thenApply(this::getNotebooksHandler).otherwise((req, res) -> {
+                            throw new HttpException("invalid content-type", Http.Status.NOT_ACCEPTABLE_406);
+                        }))
+
+                .get("/revisions/{revID}/notebooks/{notebookID}", RequestPredicate.create()
+                        .accepts(APPLICATION_NOTEBOOK_JSON, APPLICATION_JSON)
+                        .thenApply(this::getNotebookHandler)
+                        .otherwise((req, res) -> {
+                            throw new HttpException("invalid content-type", Http.Status.NOT_ACCEPTABLE_406);
+                        })
+                )
+
+                .get("/revisions/{revID}/notebooks/{notebookID}/inputs", this::getNotebookInputsHandler)
+                .get("/revisions/{revID}/notebooks/{notebookID}/outputs", this::getNotebookOutputsHandler)
+                .get("/revisions/{revID}/notebooks/{notebookID}/previous", this::getPreviousNotebooksHandler)
+                .get("/revisions/{revID}/notebooks/{notebookID}/next", this::getNextNotebooksHandler)
+
                 .put("/rev/{rev}", this::putRevisionHandler)
                 .get("/rev/{rev}", this::getRevisionHandler)
                 .delete("/rev/{rev}", this::deleteRevisionHandler)
         ;
+    }
+
+    private void getRepositoriesHandler(ServerRequest request, ServerResponse response) {
+        response.status(Http.Status.OK_200).send(List.of(
+                new Repository("foo"),
+                new Repository("bar")
+        ));
+    }
+
+    private void getRevisionsHandler(ServerRequest request, ServerResponse response) {
+        response.status(Http.Status.OK_200).send(List.of(
+                new Revision("foo"),
+                new Revision("bar")
+        ));
+    }
+
+    private void getNotebooksHandler(ServerRequest request, ServerResponse response) {
+        var nb1 = new Notebook();
+        nb1.fileName = "/foo";
+        var nb2 = new Notebook();
+        nb2.fileName = "/bar";
+        var nb3 = new Notebook();
+        nb3.fileName = "/foo/bar";
+        var nb4 = new Notebook();
+        nb4.fileName = "/bar/foo";
+
+        response.status(Http.Status.OK_200).send(List.of(
+                nb1, nb2, nb3, nb4
+        ));
+    }
+
+    private void getNotebooksDAGHandler(ServerRequest request, ServerResponse response) {
+        var nb1 = new Notebook();
+        nb1.fileName = "/foo";
+        var nb2 = new Notebook();
+        nb2.fileName = "/bar";
+        var nb3 = new Notebook();
+        nb3.fileName = "/foo/bar";
+        var nb4 = new Notebook();
+        nb4.fileName = "/bar/foo";
+
+        response.status(Http.Status.OK_200).send(List.of(
+                new Dependency(nb1, nb2),
+                new Dependency(nb1, nb3),
+                new Dependency(nb2, nb4),
+                new Dependency(nb3, nb4),
+                new Dependency(nb1, nb4)
+        ));
+    }
+
+    private void getNotebookHandler(ServerRequest request, ServerResponse response) {
+        var nb1 = new Notebook();
+        nb1.fileName = "/foo";
+        response.status(Http.Status.OK_200).send(nb1);
+    }
+
+    private void getNotebookOutputsHandler(ServerRequest request, ServerResponse response) {
+        request.next(new UnsupportedOperationException("TODO"));
+    }
+
+    private void getNotebookInputsHandler(ServerRequest request, ServerResponse response) {
+        request.next(new UnsupportedOperationException("TODO"));
+    }
+
+    private void getNextNotebooksHandler(ServerRequest request, ServerResponse response) {
+        request.next(new UnsupportedOperationException("TODO"));
+    }
+
+    private void getPreviousNotebooksHandler(ServerRequest request, ServerResponse response) {
+        request.next(new UnsupportedOperationException("TODO"));
     }
 
     private void putRevisionHandler(ServerRequest request, ServerResponse response) {
---
+++ src/test/java/no/ssb/dapla/blueprint/BlueprintServiceTest.java
@@ -0,0 +1,139 @@
+package no.ssb.dapla.blueprint;
+
+import io.helidon.common.http.Http;
+import io.helidon.common.http.MediaType;
+import io.helidon.config.Config;
+import io.helidon.webclient.WebClient;
+import io.helidon.webclient.WebClientResponse;
+import io.helidon.webserver.WebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(HelidonConfigExtension.class)
+class BlueprintServiceTest {
+
+    private static WebServer server;
+    private static WebClient client;
+
+    @BeforeAll
+    static void beforeAll(Config config) throws InterruptedException, ExecutionException, TimeoutException, NoSuchAlgorithmException {
+        server = new BlueprintApplication(config).get(WebServer.class);
+        server.start().get(10, TimeUnit.SECONDS);
+        client = WebClient.builder().baseUri("http://localhost:" + server.port()).build();
+    }
+
+    @AfterAll
+    static void afterAll() throws InterruptedException, ExecutionException, TimeoutException {
+        server.shutdown().get(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void testListRepository() {
+        var response = client.get()
+                .path("/api/v1/repository")
+                .accept(MediaType.APPLICATION_JSON).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.OK_200);
+    }
+
+    @Test
+    void testListRevision() {
+        var response = client.get()
+                .path("/api/v1/repository/repoID/revisions")
+                .accept(MediaType.APPLICATION_JSON).submit();
+        assertThat(response).succeedsWithin(1, TimeUnit.SECONDS);
+
+        response = client.get()
+                .path("/api/v1/repository/repoID/revisions")
+                .accept(BlueprintService.APPLICATION_REVISION_JSON).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.OK_200);
+
+        response = client.get()
+                .path("/api/v1/repository/repoID/revisions")
+                .accept(MediaType.TEXT_HTML).submit();
+        // See https://github.com/joel-costigliola/assertj-core/issues/1214#issuecomment-675440543
+        assertThat(response).succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.NOT_ACCEPTABLE_406);
+    }
+
+    @Test
+    void testListNotebooks() {
+        var response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks")
+                .accept(MediaType.APPLICATION_JSON).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.OK_200);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks")
+                .accept(BlueprintService.APPLICATION_NOTEBOOK_JSON).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.OK_200);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks")
+                .accept(BlueprintService.APPLICATION_DAG_JSON).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.OK_200);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks")
+                .accept(MediaType.TEXT_HTML).submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.NOT_ACCEPTABLE_406);
+    }
+
+    @Test
+    void testTodos() {
+        var response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks/{notebookID}/inputs").submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.INTERNAL_SERVER_ERROR_500);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks/{notebookID}/outputs").submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.INTERNAL_SERVER_ERROR_500);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks/{notebookID}/previous").submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.INTERNAL_SERVER_ERROR_500);
+
+        response = client.get()
+                .path("/api/v1/revisions/{revID}/notebooks/{notebookID}/next").submit();
+        assertThat(response)
+                .succeedsWithin(1, TimeUnit.SECONDS)
+                .extracting(WebClientResponse::status)
+                .isEqualTo(Http.Status.INTERNAL_SERVER_ERROR_500);
+    }
+}
